ab9922ea9fb473a6e31430152800251cef4e98fe
Adrian Sivilha <108024375+adrian-siv@users.noreply.github.com>
2023-07-19 20:10:44 -0300

    fix: switch h2 content to make it more reusable

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <Banner />
-      <Formulario />
+      <Formulario title="Preencha os dados para criar o card do colaborador" />
     </div>
   );
 }
--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,11 +1,11 @@
 import "./Formulario.css";
 import CampoTexto from "../CampoTexto"
 
-const Formulario = () => {
+const Formulario = (props) => {
 	return (
 		<section className="formulario">
 			<form>
-                <h2>Preencha os dados para criar o card do colaborador</h2>
+                <h2>{props.title}</h2>
 				<CampoTexto label="Nome" placeholder="Digite seu nome" />
 				<CampoTexto label="Cargo" placeholder="Digite seu cargo" />
 				<CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
