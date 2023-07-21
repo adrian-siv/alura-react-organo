3155a76cbee8b02fac21c532b0e6b6a242066758
Adrian Sivilha <108024375+adrian-siv@users.noreply.github.com>
2023-07-21 20:18:25 -0300

    feat: add select "times"

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,7 +1,18 @@
 import "./Formulario.css";
 import CampoTexto from "../CampoTexto"
+import ListaSuspensa from "../ListaSuspensa"
 
 const Formulario = (props) => {
+	const Times = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'DevOps',
+        'UX & Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
+	
 	return (
 		<section className="formulario">
 			<form>
@@ -9,6 +20,7 @@ const Formulario = (props) => {
 				<CampoTexto label="Nome" placeholder="Digite seu nome" />
 				<CampoTexto label="Cargo" placeholder="Digite seu cargo" />
 				<CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
+				<ListaSuspensa label="Time" itens={Times}/>
 			</form>
 		</section>
 	);
