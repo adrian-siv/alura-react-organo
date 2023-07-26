c068f8cc718cc6734435d3c0b83c8d21249fdeaa
Adrian Sivilha <108024375+adrian-siv@users.noreply.github.com>
2023-07-26 16:02:22 -0300

    fix: add conditional rendering on teams

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -3,7 +3,7 @@ import './Time.css';
 
 const Time = ({corDeFundo, corDeDestaque, nome, colaboradores}) => {
     return (
-        <section className='time' style={{backgroundColor: corDeFundo}}>
+        (colaboradores.length > 0) && <section className='time' style={{backgroundColor: corDeFundo}}>
             <h3 style={{borderColor: corDeDestaque}}>{nome}</h3>
             <div className='caixaDeColaboradores'>            
                 {colaboradores.map(colaborador => <Colaborador corDoTime={corDeDestaque} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem}/>)}
