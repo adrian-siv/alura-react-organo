a323d9a223d837059c80b8896c7ce125d011c244
Adrian Sivilha <108024375+adrian-siv@users.noreply.github.com>
2023-07-26 10:12:57 -0300

    feat: add "Colaboradores" cards

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css';
+
+const Colaborador = () => {
+    return (
+        <div className='colaborador'>
+            <div className='superior'>
+                <img src='https://github.com/adrian-siv.png' alt='Foto do colaborador Adrian'/>
+            </div>
+            <div className='inferior'>
+                <h4>Adrian Sivilha</h4>
+                <h5>Analista e Desenvolvedor de Sistemas</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador;
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,9 +1,16 @@
+import Colaborador from '../Colaborador';
 import './Time.css';
 
 const Time = (props) => {
     return (
         <section style={{backgroundColor: props.corDeFundo}} className='time'>
             <h3 style={{borderColor: props.corDeDestaque}}>{props.nome}</h3>
+            <div className='caixaDeColaboradores'>
+                <Colaborador/>
+                <Colaborador/>
+                <Colaborador/>
+                <Colaborador/>
+            </div>
         </section>
     )
 }
