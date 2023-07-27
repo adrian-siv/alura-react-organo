7dedc221f7f49cd9c9aa8206b1c61f5d20805654
Adrian Sivilha <108024375+adrian-siv@users.noreply.github.com>
2023-07-27 10:27:00 -0300

    fix: add key to "colaboradores" elements

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -6,7 +6,7 @@ const Time = ({corDeFundo, corDeDestaque, nome, colaboradores}) => {
         (colaboradores.length > 0) && <section className='time' style={{backgroundColor: corDeFundo}}>
             <h3 style={{borderColor: corDeDestaque}}>{nome}</h3>
             <div className='caixaDeColaboradores'>            
-                {colaboradores.map(colaborador => <Colaborador corDoTime={corDeDestaque} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem}/>)}
+                {colaboradores.map(colaborador => <Colaborador key={colaborador.nome} corDoTime={corDeDestaque} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem}/>)}
             </div>
         </section>
     )
